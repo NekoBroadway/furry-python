00fefa2a908d1dcc180819b5c14e824f52a7b6b6
NekoBroadway <yurafeschak@gmail.com>
2022-01-22 13:53:36 +0100

    Merge branch 'master' of https://github.com/NekoBroadway/furry-python

--- PythonApplication1/app.py
+++ PythonApplication1/app.py
@@ -1,1 +1,16 @@
-#There were some excercises from Python
+#Exc 1 - Print text below in similar formating
+#
+#twinkle, twinkle, little star,
+#	how i wonder what you are! 
+#		up above the world so high,   		
+#		like a diamond in the sky. 
+#twinkle, twinkle, little star, 
+#	how i wonder what you are
+
+row_1 = "Twinkle, twinkle, little star,"
+row_2 = "\n\tHow i wonder what you are"
+row_3 = "\n\t\tUp above the world so high,"
+row_4 = "\n\t\tLike a diamond in the sky."
+
+res = row_1 + row_2 + '!' + row_3 + row_4 + '\n' + row_1 + row_2
+print(res)
