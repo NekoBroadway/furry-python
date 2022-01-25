1f994ae671347af50f193b17b591acd759b16357
NekoBroadway <yurafeschak@gmail.com>
2022-01-25 23:49:33 +0100

    exc-10 - solution

--- PythonApplication1/app.py
+++ PythonApplication1/app.py
@@ -1,7 +1,6 @@
-#Exc 9 - Write a Python program to display the examination schedule. (extract the date from exam_st_date).
-#exam_st_date = (11, 12, 2014)
+#Exc 10 - Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
 
-exam_st_date = (11, 12, 2014)
-ex = exam_st_date
+n = input("Enter an integer (n): ")
+res = int(n) + int(n + n) + int(n + n + n)
 
-print("The examination will start from: {0}/{1}/{2}".format(ex[0], ex[1], ex[2]))
+print(f"{n} + {n}{n} + {n}{n}{n} = {res}")
