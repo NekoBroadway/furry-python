bf4357d10c7cea566f67fc368f61a6fe5f6d8412
NekoBroadway <yurafeschak@gmail.com>
2022-01-25 20:26:51 +0100

    exc-7 - solution

--- PythonApplication1/app.py
+++ PythonApplication1/app.py
@@ -1,10 +1,11 @@
-#Exc 6 - Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers
+#Exc 7 - Write a Python program to accept a filename from the user and print the extension of that
 
-seq = input("Enter some number separated with commas: ")
+import os
 
-my_list = seq.split(", ")
+path = input("Enter full path to file: ")
 
-my_tuple = tuple(my_list)
-
-print(f"List - {my_list}")
-print(f"Tuple - {my_tuple}")
+if os.path.exists(path):
+    ex = os.path.basename(path).split(".") 
+    print(f"Type of your file is \"{ex[1]}\"")
+else:
+    print("This file does not exists!")
