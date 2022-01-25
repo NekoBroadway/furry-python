ae8e05a1c564e11ed16d8d5dca684e41b51160c9
NekoBroadway <yurafeschak@gmail.com>
2022-01-25 20:38:42 +0100

    exc-9 - solution

--- PythonApplication1/app.py
+++ PythonApplication1/app.py
@@ -1,8 +1,7 @@
-#Exc 8 - Write a Python program to display the first and last colors from the following list
-#color_list = ["Red", "Green", "White", "Black"]
+#Exc 9 - Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+#exam_st_date = (11, 12, 2014)
 
-color_list = ["Red", "Green", "White", "Black"]
+exam_st_date = (11, 12, 2014)
+ex = exam_st_date
 
-print(f"Color list - {color_list}")
-print(f"First color of colorlist: {color_list[0]}")
-print(f"Last color of colorlist: {color_list[-1]}")
+print("The examination will start from: {0}/{1}/{2}".format(ex[0], ex[1], ex[2]))
