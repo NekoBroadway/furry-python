28388369afa2288d2c0ffd5f6f6eea92f1313f42
NekoBroadway <yurafeschak@gmail.com>
2022-02-09 17:34:29 +0100

    Generator wyrazów do zadania od Gammasoft

--- PythonApplication1/app.py
+++ PythonApplication1/app.py
@@ -1,1 +1,18 @@
-#Here will be some excercises from Python
+import random as r
+
+def genRandFloats(path, size):
+    with open(path, "w") as somefile:
+        for i in range(size):
+            n1 = r.random() * 1000000
+            n2 = r.random() * 1000000
+            n3 = r.random() * 1000000
+            form = "{};{};{}\n".format(n1, n2, n3)
+            somefile.write(form)
+
+file1 = "lorem1.txt"
+file2 = "lorem2.txt"
+file3 = "lorem3.txt"
+
+genRandFloats(file1, 100)
+genRandFloats(file2, 1000)
+genRandFloats(file3, 10000)
