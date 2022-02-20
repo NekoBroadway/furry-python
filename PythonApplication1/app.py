5f330270f821db85bc04b782207406aef8a6303c
NekoBroadway <yurafeschak@gmail.com>
2022-02-20 23:10:07 +0100

    Added whitespace filter

--- PythonApplication1/app.py
+++ PythonApplication1/app.py
@@ -3,8 +3,8 @@ import random as r
 def genRandFloats(path, size):
     with open(path, "w") as sFile:
         for i in range(size):
-            n1 = int(r.random() * pow(10, r.randint(1, 100)))
-            n2 = int(r.random() * pow(10, r.randint(1, 100)))
+            n1 = int(r.random() * pow(10, r.randint(0, 30)))
+            n2 = int(r.random() * pow(10, r.randint(0, 30)))
             
             if (i % 2 == 0):
                 n3 = n1 + n2
@@ -20,11 +20,13 @@ def compareFileNumbers(path):
         f = 0
 
         num_list = sFile.read().split("\n")
+        if num_list[(len(num_list) - 1)] == "":
+            num_list.pop()
 
         for i in range(len(num_list)):
                 num_list[i] = num_list[i].split(";")
 
-        for i in range(len(num_list) - 1):
+        for i in range(len(num_list)):
             if int(num_list[i][0]) + int(num_list[i][1]) == int(num_list[i][2]):
                 t += 1
             else:
@@ -41,10 +43,11 @@ file4 = "lorem4.txt"
 genRandFloats(file1, 100)
 genRandFloats(file2, 1000)
 genRandFloats(file3, 10000)
-genRandFloats(file4, pow(10, 6))
+genRandFloats(file4, 100000)
 
 
 compareFileNumbers(file1)
 compareFileNumbers(file2)
 compareFileNumbers(file3)
 compareFileNumbers(file4)
+compareFileNumbers("D:/Learning/4Work/Gammasoft/src/feszczak/mini.txt")
