794030d5100e2a4eb302fa76380198cbaf821e41
NekoBroadway <yurafeschak@gmail.com>
2022-02-20 14:06:44 +0100

    Generowanie dużych losowych liczb całkowitych oraz ich sumowanie

--- PythonApplication1/app.py
+++ PythonApplication1/app.py
@@ -1,18 +1,50 @@
 import random as r
 
 def genRandFloats(path, size):
-    with open(path, "w") as somefile:
+    with open(path, "w") as sFile:
         for i in range(size):
-            n1 = r.random() * 1000000
-            n2 = r.random() * 1000000
-            n3 = r.random() * 1000000
+            n1 = int(r.random() * pow(10, r.randint(1, 100)))
+            n2 = int(r.random() * pow(10, r.randint(1, 100)))
+            
+            if (i % 2 == 0):
+                n3 = n1 + n2
+            else:
+                n3 = n1 + n2 + 1 
+
             form = "{};{};{}\n".format(n1, n2, n3)
-            somefile.write(form)
+            sFile.write(form)
+
+def compareFileNumbers(path):
+    with open(path, "r") as sFile:
+        t = 0
+        f = 0
+
+        num_list = sFile.read().split("\n")
+
+        for i in range(len(num_list)):
+                num_list[i] = num_list[i].split(";")
+
+        for i in range(len(num_list) - 1):
+            if int(num_list[i][0]) + int(num_list[i][1]) == int(num_list[i][2]):
+                t += 1
+            else:
+                f += 1
+
+        print(f"TRUE - {t}")
+        print(f"FALSE - {f}")
 
 file1 = "lorem1.txt"
 file2 = "lorem2.txt"
 file3 = "lorem3.txt"
+file4 = "lorem4.txt"
 
 genRandFloats(file1, 100)
 genRandFloats(file2, 1000)
 genRandFloats(file3, 10000)
+genRandFloats(file4, pow(10, 6))
+
+
+compareFileNumbers(file1)
+compareFileNumbers(file2)
+compareFileNumbers(file3)
+compareFileNumbers(file4)
